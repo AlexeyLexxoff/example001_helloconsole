1501e38bdef98670b0e8c031249919ba42dbdd85
AlexeyLexxoff <djlexxoff@gmail.com>
2023-09-06 01:23:10 +0300

    add new chapter3_4

---
+++ seminarc#_3/Program.cs
@@ -0,0 +1,12 @@
+﻿//программа принеманнт одно число на выходе все целые числа в промежутке
+
+Console.Write("Ener number : ");
+string strNum = Console.ReadLine();
+int Num = int.Parse(strNum);
+int Num0 = Num * -1;
+
+while (Num0 <= Num)
+{
+    Console.WriteLine(Num0);
+    Num0 ++;
+}
---
+++ seminarc#_4/Program.cs
@@ -0,0 +1,11 @@
+﻿//
+
+
+
+Console.Write("Ener number 3x : ");
+int Num = int.Parse(Console.ReadLine());
+if(Num >= 100 && Num <= 999)
+{
+   Console.WriteLine(Num % 10);
+}
+else Console.WriteLine("not corrected");
