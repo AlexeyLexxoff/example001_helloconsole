6b4b00816f6b02ad169b13e8ad2d3229ff9af0b5
AlexeyLexxoff <djlexxoff@gmail.com>
2023-09-06 00:12:02 +0300

    add new chapter1

--- semiarc#_1/Program.cs
+++ semiarc#_1/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿///проверка   первого числа квадрата второго
+
+
+Console.Write("Ener number : ");
+string strNum = Console.ReadLine();
+int num1 = int.Parse(strNum);
+
+Console.Write("Ener number : ");
+strNum = Console.ReadLine();
+int num2 = int.Parse(strNum);
+
+int sqr = num2 * num2;
+
+if (sqr == num1)
+{
+    System.Console.WriteLine("Yes");
+}
+else
+{
+    System.Console.WriteLine("No");
+}
