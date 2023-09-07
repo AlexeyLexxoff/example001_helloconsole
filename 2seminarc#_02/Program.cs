581d89ace663f58189a36531e402e8815bcd77b4
AlexeyLexxoff <djlexxoff@gmail.com>
2023-09-07 21:17:06 +0300

    add new seminar

--- 2seminarc#_02/Program.cs
+++ 2seminarc#_02/Program.cs
@@ -1,3 +1,11 @@
 ﻿//Рандомное 3х значное число и удаление вторпой цифры этого числа.
 
-int num = new Random().Next(100, 1000);
+int num = new Random().Next(100, 999);
+Console.WriteLine(num);
+
+int num1 = num / 100;
+int num2 = num % 10;
+
+int rez = num1 * 10 + num2;
+
+System.Console.WriteLine(rez);
