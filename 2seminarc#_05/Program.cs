71668a5d52cbdd655088f4448b311c7c25d878b9
AlexeyLexxoff <djlexxoff@gmail.com>
2023-09-09 15:17:43 +0300

    add new example

---
+++ 2seminarc#_05/Program.cs
@@ -0,0 +1,15 @@
+﻿System.Console.WriteLine("введите первое число: ");
+string number1 = System.Console.ReadLine();
+int num1 = int.Parse(number1);
+System.Console.WriteLine("введите второе число ");
+string number2 = System.Console.ReadLine();
+int num2 = int.Parse(number2);
+
+if (num1 * num1 == num2 || num2 * num2 == num1)
+{
+    System.Console.WriteLine("да");
+}
+else
+{
+    System.Console.WriteLine("нет");
+}
