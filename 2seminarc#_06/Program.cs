05312f416ae1687b103cc45140bc66ebc95e020f
AlexeyLexxoff <djlexxoff@gmail.com>
2023-09-09 15:40:13 +0300

    add new example

---
+++ 2seminarc#_06/Program.cs
@@ -0,0 +1,14 @@
+﻿System.Console.WriteLine("введите трехзначно число: ");
+string number1 = System.Console.ReadLine();
+int num = int.Parse(number1);
+if (num < 100 || num > 999)
+{
+    System.Console.WriteLine($"число неврно {num}");
+}
+else
+{
+   // int num1 = num % 10;
+   // num = num - num1;
+    int num2 = (num % 100) / 10;
+    System.Console.WriteLine($"Вторая цифра: {num2}");
+}
