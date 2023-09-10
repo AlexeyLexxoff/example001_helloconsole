214d679ea5428274eb31b2b6871aa639e7a291ac
AlexeyLexxoff <djlexxoff@gmail.com>
2023-09-11 01:02:17 +0300

    new sem

---
+++ 3seminarc#_03/Program.cs
@@ -0,0 +1,17 @@
+﻿int GetNumber(string message)
+{
+    System.Console.WriteLine($"Введите координату {message} : ");
+    int num = Convert.ToInt32(System.Console.ReadLine());
+    return num;
+}
+int numX1 = GetNumber("X1");
+int numY1 = GetNumber("Y1");
+int numX2 = GetNumber("X2");
+int numY2 = GetNumber("Y2");
+
+double numX = Math.Pow((numX2 - numX1), 2); // возведение в степеень
+double numY = Math.Pow((numY2 - numY1), 2); //
+
+double result = Math.Sqrt(numX + numY); //корень
+
+System.Console.WriteLine($"Результат равен {Math.Round(result, 2)}");
---
+++ 3seminarc#_04/Program.cs
@@ -0,0 +1,10 @@
+﻿System.Console.WriteLine("Введите число: ");
+string number = System.Console.ReadLine();
+int num1 = int.Parse(number);
+
+int num2 = 1;
+while (num2 <= num1)
+{
+    System.Console.WriteLine(num2 * num2);
+    num2++;
+}
