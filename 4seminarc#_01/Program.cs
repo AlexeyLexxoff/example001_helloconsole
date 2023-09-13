8e82750bbf22b6d53b87fba51193d502fb6d54d1
AlexeyLexxoff <djlexxoff@gmail.com>
2023-09-13 22:35:23 +0300

    add new less

---
+++ 4seminarc#_01/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 24: Напишите программу, которая
+// принимает на вход число (А) и выдаёт сумму чисел
+// от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+int GetNumber(string message)
+{
+    System.Console.WriteLine($"введите число {message}");
+    int num = Convert.ToInt32(System.Console.ReadLine());
+    return num;
+}
+
+int GetSum(int num)
+{
+    int sum = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        sum +=i;
+    }
+    return sum;
+}
+// int numA = GetNumber("A");
+// int sum = GetSum(numA);
+// System.Console.WriteLine($"sum = {sum}");
+
+int num = 10;
+int num2 = num++;// num2 = 10; num = 10 + 1 = 11.
+System.Console.WriteLine($"num = {num} ; num2 = {num2}");
+
+int num3 = ++num;// num = 11 + 1 = 12; num3 = num = 12
+System.Console.WriteLine($"num = {num} ; num3 = {num3}");
--- hw_c#3/Program.cs
+++ hw_c#3/Program.cs
@@ -1,44 +1,109 @@
-﻿// using System;
+﻿// // using System;
+
+// // public class Answer
+// // {
+// //     static bool IsPalindrome(int number)
+// //     {
+// //         if (number >= 10000 && number <= 99999)
+// //         {
+// //             int originalNumber = number;
+// //             int reversedNumber = 0;
+
+// //             while (number != 0)
+// //             {
+// //                 int digit = number % 10;
+// //                 reversedNumber = reversedNumber * 10 + digit;
+// //                 number = number / 10;
+// //             }
+
+// //             return originalNumber == reversedNumber;
+// //         }
+// //         else
+// //         {
+// //             Console.WriteLine("Number is not five-digit");
+// //             return false;
+// //         }
+// //     }
+
+// //     static void Main(string[] args)
+// //     {
+// //         int number;
+
+// //         if (args.Length >= 1)
+// //         {
+// //             number = int.Parse(args[0]);
+// //         }
+// //         else
+// //         {
+// //             number = 234322;
+// //         }
+
+// //         bool result = IsPalindrome(number);
+// //         Console.WriteLine(result);
+// //     }
+// // }
+
+
 
 // public class Answer
 // {
-//     static bool IsPalindrome(int number)
+//     static void ShowCube(int N)
 //     {
-//         if (number >= 10000 && number <= 99999)
+//         for (int i = 1; i <= N; i++)
 //         {
-//             int originalNumber = number;
-//             int reversedNumber = 0;
-
-//             while (number != 0)
-//             {
-//                 int digit = number % 10;
-//                 reversedNumber = reversedNumber * 10 + digit;
-//                 number = number / 10;
-//             }
-
-//             return originalNumber == reversedNumber;
-//         }
-//         else
-//         {
-//             Console.WriteLine("Number is not five-digit");
-//             return false;
+//             int cube = i * i * i;
+//             Console.WriteLine(cube);
 //         }
 //     }
 
 //     static void Main(string[] args)
 //     {
-//         int number;
+//         int N;
 
 //         if (args.Length >= 1)
 //         {
-//             number = int.Parse(args[0]);
+//             N = int.Parse(args[0]);
 //         }
 //         else
 //         {
-//             number = 234322;
+//             N = 6;
 //         }
 
-//         bool result = IsPalindrome(number);
-//         Console.WriteLine(result);
+//         ShowCube(N);
 //     }
 // }
+
+
+csharp
+using System;
+
+public class Answer
+{
+    static double DistanceBetweenPoints(int[] pointA, int[] pointB)
+    {
+        if (pointA.Length != 3 || pointB.Length != 3)
+        {
+            throw new ArgumentException("Invalid number of coordinates. Expected 3 coordinates for each point.");
+        }
+
+        int x1 = pointA[0];
+        int y1 = pointA[1];
+        int z1 = pointA[2];
+
+        int x2 = pointB[0];
+        int y2 = pointB[1];
+        int z2 = pointB[2];
+
+        double distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+        return distance;
+    }
+
+    static void Main(string[] args)
+    {
+        int[] pointA = { 1, 2, 3 };
+        int[] pointB = { 4, 5, 6 };
+
+        double distance = DistanceBetweenPoints(pointA, pointB);
+        Console.WriteLine(distance);
+    }
+}    
