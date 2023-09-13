ab9b43e3dbb75feff032d18bd9497b705ff5eac7
AlexeyLexxoff <djlexxoff@gmail.com>
2023-09-13 23:32:14 +0300

    add new less

--- 4seminarc#_02/Program.cs
+++ 4seminarc#_02/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 26: Напишите программу, которая
+// принимает на вход число и выдаёт количество цифр
+// в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+
+
+int GetNumber(string message)
+{
+    System.Console.WriteLine($"введите число {message}");
+    int num = Convert.ToInt32(System.Console.ReadLine());
+    return num;
+}
+
+int GetCount(int num)
+{
+    int count = 0;
+    for (; 0 < num; num /= 10)
+    {
+        count = count + 1;
+    }
+    return count;
+}
+int num = GetNumber("A");
+int resoult = GetCount(num);
+System.Console.WriteLine($"количество знаков  = {resoult}");
+
