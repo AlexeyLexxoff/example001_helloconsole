375230f07c34869fe70a340ec4e6249ae1aed167
AlexeyLexxoff <djlexxoff@gmail.com>
2023-09-13 20:27:58 +0300

    add hw_3 polindrome

--- hw_c#3/Program.cs
+++ hw_c#3/Program.cs
@@ -1,37 +1,44 @@
-﻿using System;
+﻿// using System;
 
-public class Answer
-{
-    static bool IsPalindrome(int number)
-    {
-        while (number > 0)
-        {
-            digits.Add(number % 10);
-            number /= 10;
-        }
+// public class Answer
+// {
+//     static bool IsPalindrome(int number)
+//     {
+//         if (number >= 10000 && number <= 99999)
+//         {
+//             int originalNumber = number;
+//             int reversedNumber = 0;
 
-        return digits;// Введите свое решение ниже
+//             while (number != 0)
+//             {
+//                 int digit = number % 10;
+//                 reversedNumber = reversedNumber * 10 + digit;
+//                 number = number / 10;
+//             }
 
+//             return originalNumber == reversedNumber;
+//         }
+//         else
+//         {
+//             Console.WriteLine("Number is not five-digit");
+//             return false;
+//         }
+//     }
 
-    }
+//     static void Main(string[] args)
+//     {
+//         int number;
 
-    // Не удаляйте и не меняйте метод Main! 
-    static public void Main(string[] args)
-    {
-        int number;
+//         if (args.Length >= 1)
+//         {
+//             number = int.Parse(args[0]);
+//         }
+//         else
+//         {
+//             number = 234322;
+//         }
 
-        if (args.Length >= 1)
-        {
-            number = int.Parse(args[0]);
-        }
-        else
-        {
-            // Здесь вы можете поменять значения для отправки кода на Выполнение
-            number = 64546;
-        }
-
-        // Не удаляйте строки ниже
-        bool result = IsPalindrome(number);
-        System.Console.WriteLine($"{result}");
-    }
-}
+//         bool result = IsPalindrome(number);
+//         Console.WriteLine(result);
+//     }
+// }
