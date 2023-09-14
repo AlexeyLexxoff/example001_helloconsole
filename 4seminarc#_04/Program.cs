21aaa2e00a682a75a7f56fecf1a5e8f3aeb45828
AlexeyLexxoff <djlexxoff@gmail.com>
2023-09-14 20:52:27 +0300

    add new less

---
+++ 4seminarc#_04/Program.cs
@@ -0,0 +1,38 @@
+﻿// Напишите программу, которая
+// выводит массив из 8 элементов, заполненный
+// нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+
+// void Arrey(int[] collection)
+// {
+//     int length = collection.Length;
+//     int index = 0;
+//     while (index < length)
+//     {
+//         collection[index] = new Random().Next(0, 1);
+//         index++;
+//     }
+
+// }
+
+
+int[] GetArrey(int size, int min, int max)
+{
+    int[] arrey = new int[size];
+    Random rand = new Random();
+    for (int index = 0; index < size; i++)
+    {
+        array[index] = rand.Next(min, max + 1);
+    }
+    return arrey;
+}
+
+void Print(int[]arr);
+{
+    int length = arr.Length;
+    for (int i = 0; i < length; i++)
+    {
+        System.Console.WriteLine($" {arr[i]} ");
+    }
+}
