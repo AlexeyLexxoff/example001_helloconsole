93ed9db7a144b810f3dfa6c23bbe5a94a48cd889
AlexeyLexxoff <djlexxoff@gmail.com>
2023-09-20 23:51:17 +0300

    add new lesson

---
+++ 6seminarc#_03/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 45: Напишите программу, которая будет создавать копию
+// заданного массива с помощью поэлементного копирования.
+
+int[] FillArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rand = new Random();
+    {
+        for (int i = 0; i < size; i++)
+        {
+            array[i] = rand.Next(min, max + 1);
+        }
+    }
+    return array;
+}
+int[] array = FillArray(8, 1, 10);
+System.Console.WriteLine(string.Join(", ", array));
+
+int[] CopyArray(int[] array)
+{
+    int[] array2 = new int[array.Length];
+    for (int i = 0; i < array2.Length; i++)
+    {
+        array2[i] = array[i];
+        
+    }
+    return array2;
+}
+int[] array2 = CopyArray(array);
+System.Console.WriteLine(string.Join(" ,", array2));
+
